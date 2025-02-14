5f807c20d78dc1f97f332ceba815b17fa131c1a8
Louis Scozzari <lscsmbp01@LSCSMBP-01.local>
2025-02-14 13:35:18 -0600

    Add Storage Backend

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { StorageBrowser } from '../components/StorageBrowser';
 
 Amplify.configure(outputs);
 
@@ -37,11 +38,17 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser />
+
         </main>
       )}
     </Authenticator>
   );
+}
   
 }
